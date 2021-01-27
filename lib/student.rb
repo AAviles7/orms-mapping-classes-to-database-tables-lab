b956a6c31cf97a33406c2adc9522086d6e9c2a03
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-27 11:02:05 -0500

    commit tree 5b470c3b20be1f51422bfbea11be0c29cb280f3e

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,44 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?,?)
+      SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+
+  def self.create
+    new_student = Student.new("name", 5)
+    new_student.save
+    return new_student
+  end
+
 end
